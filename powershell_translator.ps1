713d4418f37119e5d0cfbb8b3822090db53f9e33
Umut Can Arda <47474804+FlySquare@users.noreply.github.com>
2024-06-19 18:42:10 +0300

    feat: added The issue of adding duplicate sentences to the output has been resolved

--- powershell_translator.ps1
+++ powershell_translator.ps1
@@ -1,1 +1,18 @@
-$targetDirectory = "[your_project_path]"$outputFile = "results.txt"$files = Get-ChildItem -Path $targetDirectory -Recurse -Include *.php,*.blade.php | Where-Object { $_.FullName -notlike "*\vendor\*" }foreach ($file in $files) {    $content = Get-Content -Path $file.FullName -Raw    $matches = [Regex]::Matches($content, "Translation::convert\('(.+?)'\)")    foreach ($match in $matches) {        $extractedText = $match.Groups[1].Value        Add-Content -Path $outputFile -Value $extractedText    }}Write-Host "Success!"
+$targetDirectory = "[your_project_path]"
+$outputFile = "results.txt"
+
+$uniqueTexts = New-Object System.Collections.Generic.HashSet[string]
+$files = Get-ChildItem -Path $targetDirectory -Recurse -Include *.php,*.blade.php | Where-Object { $_.FullName -notlike "*\vendor\*" }
+
+foreach ($file in $files) {
+    $content = Get-Content -Path $file.FullName -Raw
+    $matches = [Regex]::Matches($content, "Translation::convert\('(.+?)'\)")
+    foreach ($match in $matches) {
+        $extractedText = $match.Groups[1].Value
+        if ($uniqueTexts.Add($extractedText)) {
+            Add-Content -Path $outputFile -Value $extractedText
+        }
+    }
+}
+
+Write-Host "Success!"
